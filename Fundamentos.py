ffa14458f1978183e41cef6c979e17bb5317d9f0
YeseniaL2025 <yessylunamoreno22@gmail.com>
2025-06-06 16:50:02 -0600

    Se agregan operadores aritméticos
    Se coloco un ejemplo de cada operador Aritmético

---
+++ Fundamentos.py
@@ -0,0 +1,53 @@
+if __name__ == "__main__":
+    print(".....Operadores aritméticos.....")
+# Suma
+    print("Operador suma, 5 + 6 =", 5 + 6)
+# Resta
+    print("Operador resta, 6 - 10 =", 6 - 10) 
+# Multiplicación
+    print("Operador multiplicación, 24 * 20 =", 24 * 20)
+# División
+    print("Operador división, 20 / 4 =", 20 / 4)
+# División entera
+    print("Operador división entera, 20 // 3 =", 20 // 3)
+# Módulo (residuo de una división)
+    print("Operador módulo, 20 % 3 =", 20 % 3)
+# Exponente
+    print("Operador exponente, 2 ** 3 =", 2 ** 3)
+
+    print(".....Operadores lógicos.....")
+# Variables de ejemplo
+    a = True
+    b = False
+# Operador AND (y)
+    print("Operador AND: True and False =", a and b)
+# Operador OR (o)
+    print("Operador OR: True or False =", a or b)
+# Operador NOT (no)
+    print("Operador NOT: not True =", not a)
+    print("Operador NOT: not False =", not b)
+
+    #1. Hacer las tablas de verdad
+
+
+    #2. 1 ejemplo de cada operador de comparación.
+    print(".....Operadores de comparación.....")
+
+# Igual a
+    print("5 == 5 →", 5 == 5)  # True
+
+# Distinto de
+    print("3 != 4 →", 3 != 4)  # True
+
+# Mayor que
+    print("10 > 7 →", 10 > 7)  # True
+
+# Menor que
+    print("2 < 5 →", 2 < 5)  # True
+
+# Mayor o igual que
+    print("6 >= 6 →", 6 >= 6)  # True
+
+# Menor o igual que
+    print("4 <= 9 →", 4 <= 9)  # True
+
